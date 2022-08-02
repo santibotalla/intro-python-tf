0c3d7f918eb1f3b970257742b4e0a902e72ea157
santibotalla <santiagobotalla@gmail.com>
2022-08-01 21:47:05 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-import os
 
-secret_key = 'secret'
-PWD = os.path.abspath(os.curdir)
-
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
